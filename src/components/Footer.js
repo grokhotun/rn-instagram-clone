c71e7b78a1ada669f8411c70cd80c448a8927a2a
Oscar <askarrussia@gmail.com>
2021-03-18 16:17:53 +0500

    Поправил карточку поста, добавил аватар в footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View, StyleSheet} from 'react-native'
+import {View, StyleSheet, Image} from 'react-native'
 import PropTypes from 'prop-types'
 
 import {Ionicons} from '@expo/vector-icons'
@@ -12,6 +12,9 @@ export function Footer({style}) {
       <Ionicons name="search" size={24} color="black" />
       <AntDesign name="instagram" size={24} color="black" />
       <Ionicons name="bookmark-outline" size={24} color="black" />
+      <View style={styles.profile}>
+        <Image style={styles.img} source={require('assets/user01.png')}></Image>
+      </View>
     </View>
   )
 }
@@ -23,6 +26,17 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center'
+  },
+  profile: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 30,
+    height: 30
+  },
+  img: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain'
   }
 })
 
--- src/components/Post.js
+++ src/components/Post.js
@@ -5,7 +5,7 @@ import {EvilIcons} from '@expo/vector-icons'
 
 export function Post() {
   return (
-    <View style={styles.postCard}>
+    <View style={styles.card}>
       <View style={styles.header}>
         <View style={styles.profile}>
           <Image style={styles.avatar} source={require('assets/user01.png')}/>
@@ -19,15 +19,20 @@ export function Post() {
         <Image source={require('assets/post.png')}/>
       </View>
       <View style={styles.footer}>
-        <EvilIcons name="heart" size={30} color="black" />
-        <EvilIcons name='comment' size={30} color='black' />
+        <View style={styles.footerActions}>
+          <EvilIcons name="heart" size={35} color="black" />
+          <EvilIcons name='comment' size={35} color='black' />
+        </View>
+        <View style={styles.footerDesc}>
+          <Text>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,</Text>
+        </View>
       </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  postCard: {
+  card: {
     justifyContent: 'space-between',
     width: '100%'
   },
@@ -58,5 +63,11 @@ const styles = StyleSheet.create({
   },
   footer: {
     padding: 10
-  }
+  },
+  footerActions: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10
+  },
+  footerDesc: {}
 })
