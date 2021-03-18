b7263ed9625b9cd7a126fa0011cecfc0e3434a77
Oscar <askarrussia@gmail.com>
2021-03-18 23:30:53 +0500

    Поправил стили в нижнем баре

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,27 @@
 import React from 'react'
-import {View, StyleSheet, Image} from 'react-native'
+import {View, StyleSheet, Image, TouchableOpacity} from 'react-native'
 import PropTypes from 'prop-types'
 
 import {Ionicons} from '@expo/vector-icons'
 import {AntDesign} from '@expo/vector-icons'
 
-export function Footer({style}) {
+export function Footer({style, navigation}) {
   return (
     <View style={{...styles.footer, ...style}}>
       <Ionicons name="home" size={24} color="black" />
       <Ionicons name="search" size={24} color="black" />
       <AntDesign name="instagram" size={24} color="black" />
       <Ionicons name="bookmark-outline" size={24} color="black" />
-      <View style={styles.profile}>
+      <TouchableOpacity style={styles.profile} onPress={() => navigation.navigate('Profile')}>
         <Image style={styles.img} source={require('assets/user01.png')}></Image>
-      </View>
+      </TouchableOpacity>
     </View>
   )
+}
+
+Footer.propTypes = {
+  style: PropTypes.object,
+  navigation: PropTypes.object
 }
 
 const styles = StyleSheet.create({
@@ -39,7 +44,3 @@ const styles = StyleSheet.create({
     resizeMode: 'contain'
   }
 })
-
-Footer.propTypes = {
-  style: PropTypes.object
-}
