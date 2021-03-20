51dde6389b34b83879324e3580e6fa3ada962aef
Oscar <askarrussia@gmail.com>
2021-03-20 17:22:17 +0500

    Начал разработку экрана поиска

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,44 @@
 import React from 'react'
+import {View, StyleSheet} from 'react-native'
+import PropTypes from 'prop-types'
+import {Header} from 'components/Header'
+import {Input} from 'src/ui/Input'
+// import {Footer} from 'components/Footer'
 
-import {View, Text} from 'react-native'
-
-export function SearchScreen() {
+export function SearchScreen({navigation}) {
   return (
-    <View>
-      <Text>Search Screen</Text>
+    <View style={styles.container}>
+      <Header style={styles.header}/>
+      <View style={styles.body}>
+        <View style={styles.searchbar}>
+          <Input
+            style={{marginBottom: 10, width: '100%'}}
+            placeholder='Поиск...'/>
+        </View>
+      </View>
     </View>
   )
 }
+
+SearchScreen.propTypes = {
+  navigation: PropTypes.object
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 30,
+    backgroundColor: '#fff',
+    justifyContent: 'space-between',
+  },
+  header: {
+    width: '100%',
+    height: '10%'
+  },
+  body: {
+    width: '100%',
+    height: '100%'
+  },
+  searchbar: {
+    padding: 10
+  }
+})
