0c775263457dec68659ca6d5e5e89375673292b1
Oscar <askarrussia@gmail.com>
2021-03-21 22:27:07 +0500

    Изменил компонент профиля

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import {TouchableOpacity, StyleSheet, Image} from 'react-native'
+import {TouchableOpacity, StyleSheet} from 'react-native'
 import PropTypes from 'prop-types'
 
+import Img from 'src/ui/Img'
 export function Avatar({style, onPress, source}) {
   return (
     <TouchableOpacity
       style={{...styles.avatar, ...style}}
       activeOpacity={0.7}
-      onPress={onPress}>
-      <Image style={styles.img} source={{uri: source}}/>
+      onPress={onPress}
+    >
+      <Img style={styles.img} source='https://www.peoples.ru/art/cinema/actor/christian_bale/N4VyjnwIscuHn.jpeg' />
     </TouchableOpacity>
   )
 }
@@ -28,10 +30,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     width: 30,
     height: 30
-  },
-  img: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover'
   }
 })
