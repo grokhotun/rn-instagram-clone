4daebe8444f0ef314c08d2d235e255eb05ee4368
Oscar <askarrussia@gmail.com>
2021-03-21 19:43:01 +0500

    Переписал компонент навигации

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -11,59 +11,90 @@ import {Avatar} from 'src/components/Avatar'
 import SignInScreen from 'src/screens/SignInScreen'
 import SignUpScreen from 'src/screens/SignUpScreen'
 import FeedScreen from 'src/screens/FeedScreen'
-import {SearchScreen} from 'src/screens/SearchScreen'
+// import {SearchScreen} from 'src/screens/SearchScreen'
 import {ProfileScreen} from 'src/screens/ProfileScreen'
+import CameraScreen from 'src/screens/CameraScreen'
+import EditScreen from 'src/screens/EditScreen'
 
-const RootStack = createStackNavigator()
+const AuthStack = createStackNavigator()
+const CameraStack = createStackNavigator()
 const Tabs = createBottomTabNavigator()
+
+function authStackScreen() {
+  return (
+    <AuthStack.Navigator>
+      <AuthStack.Screen
+        name='SignIn'
+        component={SignInScreen}
+        options={{headerShown: false, animationEnabled: false}}/>
+      <AuthStack.Screen
+        name='SignUp'
+        component={SignUpScreen}
+        options={{headerShown: false, animationEnabled: false}}/>
+    </AuthStack.Navigator>
+  )
+}
+
+function cameraStackScreen() {
+  return (
+    <CameraStack.Navigator>
+      <CameraStack.Screen
+        name='Camera'
+        component={CameraScreen}
+        options={{headerShown: false, animationEnabled: false}}/>
+      <CameraStack.Screen
+        name='Edit'
+        component={EditScreen}
+        options={{
+          title: 'Редактирование',
+          animationEnabled: false
+        }}/>
+    </CameraStack.Navigator>
+  )
+}
+
+function homeStackScreen() {
+  return (
+    <Tabs.Navigator tabBarOptions={{showLabel: false}}>
+      <Tabs.Screen
+        options={{
+          // eslint-disable-next-line react/display-name
+          tabBarIcon: () => <Ionicons name="home" size={24} color="black" />
+        }}
+        name='Feed'
+        component={FeedScreen} />
+      <Tabs.Screen
+        options={{
+          // eslint-disable-next-line react/display-name
+          tabBarIcon: () => <Ionicons name="search" size={24} color="black" />
+        }}
+        name='Search'
+        component={cameraStackScreen}/>
+      <Tabs.Screen
+        options={{
+          // eslint-disable-next-line react/display-name
+          tabBarIcon: () => <Avatar/>
+        }}
+        name='Profile'
+        component={ProfileScreen}/>
+    </Tabs.Navigator>
+  )
+}
 
 function AppNavigation({currentUser}) {
   return (
     <NavigationContainer>
       {
-        !currentUser? (
-          <RootStack.Navigator>
-            <RootStack.Screen
-              name='SignIn'
-              component={SignInScreen}
-              options={{headerShown: false, animationEnabled: false}}/>
-            <RootStack.Screen
-              name='SignUp'
-              component={SignUpScreen}
-              options={{headerShown: false, animationEnabled: false}}/>
-          </RootStack.Navigator>
-        ) : (
-            <Tabs.Navigator tabBarOptions={{showLabel: false}}>
-              <Tabs.Screen
-                options={{
-                  // eslint-disable-next-line react/display-name
-                  tabBarIcon: () => <Ionicons name="home" size={24} color="black" />
-                }}
-                name='Feed'
-                component={FeedScreen} />
-              <Tabs.Screen
-                options={{
-                  // eslint-disable-next-line react/display-name
-                  tabBarIcon: () => <Ionicons name="search" size={24} color="black" />
-                }}
-                name='Search'
-                component={SearchScreen}/>
-              <Tabs.Screen
-                options={{
-                  // eslint-disable-next-line react/display-name
-                  tabBarIcon: () => <Avatar/>
-                }}
-                name='Profile'
-                component={ProfileScreen}/>
-            </Tabs.Navigator>
-        )
+        !currentUser
+          ? authStackScreen()
+          : homeStackScreen()
       }
     </NavigationContainer>
   )
 }
 
 AppNavigation.propTypes = {
-  currentUser: PropTypes.object
+  currentUser: PropTypes.object || PropTypes.bool
 }
 
 const mapStateToProps = ({userState}) => ({
